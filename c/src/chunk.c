ca833b60bf6b16e52994b28b44bb0ec5c195bbe4
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-03-08 01:03:28 +0530

    add: line metadata

--- c/src/chunk.c
+++ c/src/chunk.c
@@ -7,17 +7,19 @@ void initChunk(Chunk *chunk)
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(uint8_t, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte)
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
 {
     // check if the current array has room for the new byte
     if (chunk->capacity < chunk->count + 1)
@@ -25,9 +27,11 @@ void writeChunk(Chunk *chunk, uint8_t byte)
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
--- c/src/main.c
+++ c/src/main.c
@@ -9,9 +9,9 @@ int main(int argc, const char *argv[])
     initChunk(&chunk);
     // this actually returns the constant's offset in the ValuesArray
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
